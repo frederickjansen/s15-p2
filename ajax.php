0eb8c51fa369cd44979b340912c2ff40ee80c57d
Frederick Jansen <frederick.jansen@gmail.com>
2014-07-08 13:29:16 -0400

    Separators are now randomly chosen. Max word length is now 10

--- ajax.php
+++ ajax.php
@@ -2,8 +2,8 @@
 
 //if (isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && strtolower( $_SERVER['HTTP_X_REQUESTED_WITH'] ) == 'xmlhttprequest')
 //{
-    $separator = '';
-    $length = 1;
+    $separator = '///..';
+    $length = 4;
     $endNum = false;
     $capitalize = false;
 
@@ -17,14 +17,26 @@
         {
             $wordlist = explode( "\n", $file );
             $password = '';
+
             for ($i=0; $i < $length; $i++)
             {
-                $password .= trim( $wordlist[rand( 0, sizeof( $wordlist ) )] );
-
-                if ($i != $length - 1)
+                $rand = rand( 0, sizeof( $wordlist ) - 1 );
+                // Capitalize words
+                if ($capitalize)
                 {
-                    $password .= $separator;
+                    $password .= trim( ucfirst( $wordlist[$rand] ) );
                 }
+                else
+                {
+                    $password .= trim( $wordlist[$rand] );
+                }
+
+                // Add separator when not the last word
+                if ($i != $length - 1 && !empty( $separator ) )
+                {
+                    $password .= $separator[rand( 0, strlen( $separator ) - 1 )];
+                }
+                // Add number to the end of the password
                 else if ($endNum)
                 {
                     $password .= rand( 0,9 );
@@ -33,7 +45,7 @@
         }
         else
         {
-            echo "file not found";
+            echo;
         }
         echo $password;
         //echo json_decode('{result: "success", password: "'.$password.'"}');
@@ -50,7 +62,11 @@ function validateOptions()
     global $separator, $length, $endNum, $capitalize;
 
     $separator = isset( $_POST['separator'] ) ? $_POST['separator'] : '';
-    $length = isset( $_POST['length'] ) ? $_POST['length'] : 4;
+    $length = isset( $_POST['length'] ) ? intval( $_POST['length'] ) : 4;
+    if ($length > 10)
+    {
+        $length = 10;
+    }
     $endNum = isset( $_POST['endNum'] ) ? true : false;
     $capitalize = isset( $_POST['capitalize'] ) ? true : false;
 }
@@ -70,7 +86,7 @@ function cleanGlobals( &$data )
         $v = str_replace( "\x00", '', $v );
         $v = str_replace( '%00', '', $v );
         
-        // No directory changes
+        // No directory changes allowed
         $v = str_replace( "../", "&#46;&#46;/", $v );
         
         $data[$k] = $v;
